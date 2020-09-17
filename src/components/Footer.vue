7306387cc00b8b67d9ef0c8a70cfe576bb89e1ad
Peterg <petercharles.ca@gmail.com>
2020-09-17 13:55:51 -0400

    Nested routes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,18 +3,24 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">
+          <router-link to="/" tag="li">
+            <a class="nav-link">
               home
               <span class="sr-only">(current)</span>
             </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">menu</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">admin</a>
-          </li>
+          </router-link>
+          <router-link to="/about" tag="li">
+            <a class="nav-link">
+              about
+              <span class="sr-only">(current)</span>
+            </a>
+          </router-link>
+          <router-link to="/admin" tag="li">
+            <a class="nav-link">
+              admin
+              <span class="sr-only">(current)</span>
+            </a>
+          </router-link>
         </ul>
       </div>
     </nav>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from './components/Home.vue'
 import Menu from './components/Menu.vue'
+import About from './components/About.vue'
+import Contact from './components/Contact.vue'
+import Delivery from './components/Delivery.vue'
+import History from './components/History.vue'
+import OrderingGuide from './components/OrderingGuide.vue'
+import Admin from './components/Admin.vue'
 import App from './App.vue'
 
 Vue.use(VueRouter)
@@ -9,7 +15,14 @@ Vue.use(VueRouter)
 const routes = [
   { path: '/', component: Home },
   { path: '/menu', component: Menu },
-  { path: '*', redirect: '/'}
+  { path: '/admin', component: Admin },
+  { path: '/about', component: About, children: [
+    { path: '/contact', component: Contact },    // /contact www.pizzaplanet.com.contact  // contact www.pizzaplanet.com/about/contact
+    { path: '/history', component: History },
+    { path: '/delivery', component: Delivery },
+    { path: '/ordering-guide', component: OrderingGuide },
+  ]},
+  { path: '*', redirect: '/'},
 ]
 
 const router = new VueRouter({
