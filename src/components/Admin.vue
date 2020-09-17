e1fca5e87d6000db2ed8df57d2430a2647957ffd
Peterg <petercharles.ca@gmail.com>
2020-09-16 23:48:00 -0400

    Listing current orders

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
   <div class="row">
     <div class="col-md-6">
       <!-- new pizza component -->
@@ -21,6 +22,40 @@
       </table>
     </div>
   </div>
+
+  <div class="row">
+    <div class="col-sm-12">
+      <h3>Current orders:</h3>
+      <table class="table table-sm">
+
+        <thead class="thead-default">
+          <tr>
+            <th>Item</th>
+            <th>Size</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+
+        <tbody>
+
+          <div class="order-number">
+            <strong><em>Order Number: 1</em></strong>
+            <button class="btn btn-outline-danger btn-sm">X</button>
+          </div>
+
+          <tr>
+            <td>Margherita</td>
+            <td>9"</td>
+            <td>1</td>
+            <td>6.95</td>
+          </tr>
+        </tbody>
+
+      </table>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
