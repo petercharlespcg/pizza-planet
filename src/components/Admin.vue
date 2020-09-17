4d6226b8f0652ce40aa91e5f6626df743f0ebd8c
Peterg <petercharles.ca@gmail.com>
2020-09-17 00:00:15 -0400

    Add new pizza component

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -2,7 +2,7 @@
 <div>
   <div class="row">
     <div class="col-md-6">
-      <!-- new pizza component -->
+      <pp-new-pizza></pp-new-pizza>
     </div>
     <div class="col-md-6">
       <h3>Menu:</h3>
@@ -59,6 +59,11 @@
 </template>
 
 <script>
-export default {};
+import NewPizza from './NewPizza'
+export default {
+  components: {
+    ppNewPizza: NewPizza
+  }
+};
 </script>
 
---
+++ src/components/NewPizza.vue
@@ -0,0 +1,54 @@
+<template>
+  <form>
+    <h3>Add new pizza:</h3>
+
+    <div class="form-group row">
+      <label class="col-sm-3">Name</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control">
+      </div>
+    </div>
+
+    <div class="form-group row">
+      <label class="col-sm-3">Description</label>
+      <div class="col-sm-9">
+        <textarea type="text" class="form-control" rows="5"></textarea>
+      </div>
+    </div>
+
+    <p><strong>Option 1:</strong></p>
+    <div class="form-group row">
+      <label class="col-sm-3">Size (")</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control">
+      </div>
+    </div>
+
+    <div class="form-group row">
+      <label class="col-sm-3">Price</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control">
+      </div>
+    </div>
+
+    <p><strong>Option 2:</strong></p>
+    <div class="form-group row">
+      <label class="col-sm-3">Size (")</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control">
+      </div>
+    </div>
+
+    <div class="form-group row">
+      <label class="col-sm-3">Price</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control">
+      </div>
+    </div>
+
+    <div class="form-group row">
+      <button type="button" class="btn btn-success btn-block">Add</button>
+    </div>
+
+  </form>
+</template>
