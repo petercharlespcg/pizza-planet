7308ec905b6076bb5198af39ce157ad6bf5d3647
Peterg <petercharles.ca@gmail.com>
2020-09-17 12:43:41 -0400

    Final components

---
+++ src/components/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Contact</h5>
+      <p class="card-text">
+        <address>
+          <strong>Pizza Planet</strong><br>
+          Address line 1<br>
+          Address line 2<br>
+          Phone: (123) 456-7890
+        </address>
+        <address>
+          <strong>Email:</strong><br>
+          <p>Contact@pizzaplanet.com</p>
+        </address>
+      </p>
+    </div>
+  </div>
+</template>
