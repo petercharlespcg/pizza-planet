9135e5c8cd1b0059bb807fb88044ed8cbb88e847
Peterg <petercharles.ca@gmail.com>
2020-09-17 12:07:35 -0400

    Signing in and out

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -21,10 +21,26 @@ import Firebase from 'firebase'
 export default {
   methods: {
     signIn() {
+      var email = document.getElementById('email').value;
+      var password = document.getElementById('password').value;
 
+      Firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        if (errorCode === 'auth/wrong-password') {
+          alert('Wrong password')
+        } else {
+          alert(errorMessage);
+        }
+      });
     },
     signOut() {
-
+      Firebase.auth().signOut().then(function() {
+        alert('logged out');
+      }).catch(function(error) {
+        alert('error.')
+      })
     }
   }
 }
