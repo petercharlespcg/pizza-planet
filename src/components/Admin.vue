26d82d00407af214bd815969f4a01e3521a3d28c
Peterg <petercharles.ca@gmail.com>
2020-09-19 23:54:42 -0400

    Lazy loading with conditional rendering

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,65 +1,6 @@
 <template>
 <div>
-<section v-if="currentUser">
-  <div class="row">
-    <div class="col-sm-12 col-md-6">
-      <pp-new-pizza></pp-new-pizza>
-    </div>
-    <div class="col-sm-12 col-md-6">
-      <h3>Menu:</h3>
-      <table class="table table-hover">
-        <thead class="thead-default">
-          <tr>
-            <th>Item</th>
-            <th>Remove from menu</th>
-          </tr>
-        </thead>
-        <tbody v-for="item in getMenuItems" :key="item['.key']">
-          <tr>
-            <td>{{ item.name }}</td>
-            <td><button class="btn btn-outline-danger btn-sm"
-                @click="removeMenuItem(item['.key'])">X</button></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="col-sm-12">
-      <h3>Current orders: {{ numberOfOrders }}</h3>
-      <table class="table table-sm" v-for="(orders, index) in getOrders" :key="orders['.key']">
-
-        <thead class="thead-default">
-          <tr>
-            <th>Item</th>
-            <th>Size</th>
-            <th>Quantity</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-
-        <tbody>
-
-          <div class="order-number">
-            <strong><em>Order Number: {{ index + 1 }}</em></strong>
-            <button class="btn btn-outline-danger btn-sm"
-              @click="removeOrderItem(orders['.key'])">X</button>
-          </div>
-
-          <tr v-for="orderItems in orders">
-            <td>{{ orderItems.name }}</td>
-            <td>{{ orderItems.size }}</td>
-            <td>{{ orderItems.quantity }}</td>
-            <td>{{ orderItems.price | currency}}</td>
-          </tr>
-        </tbody>
-
-      </table>
-    </div>
-  </div>
-</section>
-
+  <pp-admin-hidden v-if="currentUser"></pp-admin-hidden>
   <hr>
   <div class="row">
     <div class="col-sm12 col-lg-6">
@@ -70,40 +11,23 @@
 </template>
 
 <script>
-import NewPizza from './NewPizza'
 import Login from './Login.vue'
-import { mapGetters } from "vuex";
-import { dbMenuRef, dbOrdersRef } from '../firebaseConfig';
+import { mapGetters } from "vuex"
+
+//dynamic import
+const AdminHidden = () => import('./AdminHidden.vue')
 
 export default {
   components: {
-    ppNewPizza: NewPizza,
-    ppLogin: Login
+    ppLogin: Login,
+    ppAdminHidden: AdminHidden
   },
   computed: {
     ...mapGetters([
-      'numberOfOrders',
-      'getMenuItems',
-      'getOrders',
       'currentUser'
     ])
-  },
-  methods: {
-    removeMenuItem(key) {
-      dbMenuRef.child(key).remove()
-    },
-    removeOrderItem(key) {
-      dbOrdersRef.child(key).remove()
-    }
-  },
-  beforeRouteLeave: (to, from, next) => {
-    if (confirm("Have you remembered to log out") === true) {
-      next();
-    } else {
-      next(false);
-    }
   }
-};
+}
 </script>
 
 <style scoped>
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,11 @@
 import Home from './components/Home.vue'
-// import Menu from './components/Menu.vue'
-const Menu = () => import('./components/Menu.vue')
-
-// import About from './components/About.vue'
-const About = () => import(/* webpackChunkName: "footer-group" */ './components/About.vue')
-
+import Menu from './components/Menu.vue'
+import About from './components/About.vue'
 import Contact from './components/Contact.vue'
 import Delivery from './components/Delivery.vue'
 import History from './components/History.vue'
 import OrderingGuide from './components/OrderingGuide.vue'
-
-// import Admin from './components/Admin.vue'
-const Admin = () => import(/* webpackChunkName: "footer-group" */ './components/Admin.vue')
+import Admin from './components/Admin.vue'
 
 export const routes = [
   { path: '/',
